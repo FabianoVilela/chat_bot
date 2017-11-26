0a16a94edba239fd33fed29b00142ba85c7215c0
Fabiano Vilela <fabiano.vilela@gmail.com>
2017-11-26 11:15:04 -0200

    Service created

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,28 +5,6 @@ RSpec.describe App  do
   it 'get sinatra response' do
     get '/'
     expect(last_response).to be_ok
-    #expect(last_response.body).to eq('Hello World!')
+    expect(last_response.body).to eq('Hello World!')
   end
 end
-
-
-
-#ENV['RACK_ENV'] = 'test'
-
-#require 'hello_world'  # <-- your sinatra app
-#require 'rspec'
-#require 'rack/test'
-
-#describe 'The HelloWorld App' do
-#  include Rack::Test::Methods
-
-#  def app
-#    Sinatra::Application
-#  end
-
-#  it "says hello" do
-#    get '/'
-#    expect(last_response).to be_ok
-#    expect(last_response.body).to eq('Hello World')
-#  end
-#end
